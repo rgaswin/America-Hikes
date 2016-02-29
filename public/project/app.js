3670f1021d49760b22b0215682c339005db663cd
rgaswin <aswinedumails@gmail.com>
2016-02-29 04:46:56 -0500

    Added Homepage

---
+++ public/project/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by gopal on 2/29/2016.
+ */
+(function () {
+    angular
+        .module("HikerApp", ["ngroute"]);
+})();
---
+++ public/project/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gopal on 2/29/2016.
+ */
+(function () {
+    angular
+        .module("HikerApp")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+    }
+})();
