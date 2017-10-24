3935b3b65623b37d109b502cd64cb42725051b41
Aswin Gopalan <rgaswin@ccs.neu.edu>
2017-10-24 19:30:38 -0400

    server js change

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 3000;
 
 // server side 'app' reference for assignment code.
 require("./public/assignment/server/app.js")(app, db, mongoose);
@@ -56,4 +56,6 @@ app.get('/hello', function (req, res) {
     res.send('hello world');
 });
 
-app.listen(port, ipaddress);
+//app.listen(port, ipaddress);
+
+app.listen(port);
